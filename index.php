90ecc1ce1d6b5f990257590b966c4476ce69888c
Amir Roox <amirroox@yahoo.com>
2023-05-15 15:54:56 +0430

    Add Font (Vazir) + Make Header.php + Section One

--- index.php
+++ index.php
@@ -5,10 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Developer Roadmap</title>
+    <title>Developer RoadMap</title>
     <link rel="stylesheet" href="assets/css/SeedStyle.css">
+    <link rel="stylesheet" href="assets/css/Style.css">
 </head>
 <body>
-    <h1>Start</h1>
+<?php require "public/Header.php" ?>
+
+<section id="Head-Sec">
+    <br>
+    <div class="Header">
+        <h1>Developer Roadmap</h1>
+        <p>نقشه راه شما آماده است</p>
+    </div>
+</section>
+
 </body>
 </html>
+
