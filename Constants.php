fd993739f8265a808d90bd70170778751062dd3f
Amir Roox <amirroox@yahoo.com>
2023-05-23 00:43:20 +0430

    SVG LOADER Fixing (NON Beta)

--- Constants.php
+++ Constants.php
@@ -6,16 +6,6 @@ const MAIN_DIR = __DIR__ . '/';  #Main Directory
 const MAIN_SERVER = "http://localhost:63342/Persian-developer-Roadmap" . '/';  # Main Server
 const MAIN_GITHUB = "https://github.com/amirroox/Persian-Developer-Roadmap"; # Github Page
 const MAIN_SITE = "https://ro-ox.com";
-const MAIN_COLOR_CHECK = [
-    "ok" => "rgb(254,0,38,255)" ,   # Recommend
-    "replace" => "rgb(255, 153, 145)" ,  # Alternative
-    "no" =>  "rgb(153,153,153)" ,  # Other Or Not Recommend
-];
-const MAIN_COLOR_BACK = [
-    "main" => "rgb(0,255,225)" ,   # MAIN COLOR
-    "minor" => "rgb(122,255,101)" ,  # Minor
-    "road" =>  "rgb(43,120,228)" ,  # Road - Line
-];
-const CHECK_SVG = 0 ;  //Default Value
+const CHECK_SVG = 1 ;  //Default Value
 
 include "assets/vendor/Helper.php" ;
--- assets/vendor/Helper.php
+++ assets/vendor/Helper.php
@@ -1,26 +1,73 @@
 <?php
 
-function CheckLoadSVG($test,$Name_Page , $name=null): void  # Method For Change Color All SVG Road Map
+/* Const Color For Svg Road Map */
+/* Change -> Static -> MAIN_SVG_COPY */
+/* Change -> Static -> MAIN_SVG_COPY */
+$MAIN_COLOR_CHECK = [
+    "Recommend" => ["rgb(120,20,255)", "rgb(254,0,38)", "rgb(153,0,255)"],   # Recommend
+    "Alternative" => ["rgb(120, 54, 100)", "rgb(255, 153, 145)", "rgb(56,118,29)"],  # Alternative
+    "No" => ["rgb(153,153,153)", "rgb(153,153,153)", "rgb(153,153,153)"],  # Other Or Not Recommend
+];
+$MAIN_COLOR_BACK = [
+    "Main" => ["rgb(0,12,225)", "rgb(0,255,225)", "rgb(255,255,0)"],   # MAIN COLOR
+    "Minor" => ["rgb(122,255,0)", "rgb(0,255,225)", "rgb(255,229,153)"],  # Minor
+    "Road" => ["rgb(43,120,0)", "rgb(0,255,225)", "rgb(43,120,228)"],  # Road - Line
+    "Text" => ['fill="rgb(0,0,0)"', 'fill="rgb(0,0,0)"', 'fill="rgb(0,0,0)"'],  # Text
+];
+
+function CheckLoadSVG($test, $Name_Page, $name = null): void  # Method For Change Color All SVG Road Map
 {   #Checking Svg Reload
+    global $MAIN_COLOR_CHECK;
+    global $MAIN_COLOR_BACK;
+    $check = MAIN_DIR . "public/Pages/$Name_Page/bin/" . "$Name_Page$name.svg";
     # $test ==> 1 (Test)   |   0 (No Test)
-    $check = MAIN_DIR . "public/Pages/$Name_Page/bin/" ."$Name_Page$name.svg";
-    if((bool)$test) {
+    if ($test) {
         $change = file_get_contents($check);
-        if (!(bool)strpos("$change", "font-family: lalezar")) {
+//        if (strpos($change, $MAIN_COLOR_CHECK['Recommend'][2])) {
+//            #chcek Color MAINCopy
+//            $change = str_replace($MAIN_COLOR_CHECK['Recommend'][2], $MAIN_COLOR_CHECK['Recommend'][1], $change);
+//            $change = str_replace($MAIN_COLOR_CHECK['Alternative'][2], $MAIN_COLOR_CHECK['Alternative'][1], $change);
+//            $change = str_replace($MAIN_COLOR_CHECK['No'][2], $MAIN_COLOR_CHECK['No'][1], $change);
+//            #box Color MAINCopy
+//            $change = str_replace($MAIN_COLOR_BACK['Main'][2], $MAIN_COLOR_BACK['Main'][1], $change);
+//            $change = str_replace($MAIN_COLOR_BACK['Minor'][2], $MAIN_COLOR_BACK['Minor'][1], $change);
+//            $change = str_replace($MAIN_COLOR_BACK['Road'][2], $MAIN_COLOR_BACK['Road'][1], $change);
+////            $change = str_replace(MAIN_COLOR_BACK['text'][2], MAIN_COLOR_BACK['text'][1], $change);
+//
+//            $change = str_replace('font-family: balsamic' , 'font-family: lalezar', $change);
+//        }
 
-            #chcek Color
-            $change = str_replace("rgb(153,0,255)", MAIN_COLOR_CHECK['ok'], $change);
-            $change = str_replace("rgb(56,118,29)", MAIN_COLOR_CHECK['replace'], $change);
-            $change = str_replace("rgb(153,153,153)", MAIN_COLOR_CHECK['no'], $change);
+//        #chcek Color
+//        $change = str_replace($MAIN_COLOR_CHECK['Recommend'][1], $MAIN_COLOR_CHECK['Recommend'][0], $change);
+//        $change = str_replace($MAIN_COLOR_CHECK['Alternative'][1], $MAIN_COLOR_CHECK['Alternative'][0], $change);
+//        $change = str_replace($MAIN_COLOR_CHECK['No'][1], $MAIN_COLOR_CHECK['No'][0], $change);
+//
+//        #box Color
+//        $change = str_replace($MAIN_COLOR_BACK['Main'][1], $MAIN_COLOR_BACK['Main'][0], $change);
+//        $change = str_replace($MAIN_COLOR_BACK['Minor'][1], $MAIN_COLOR_BACK['Minor'][0], $change);
+//        $change = str_replace($MAIN_COLOR_BACK['Road'][1], $MAIN_COLOR_BACK['Road'][0], $change);
+////        $change = str_replace(MAIN_COLOR_BACK['text'][1], MAIN_COLOR_BACK['text'][0], $change);
 
-            #box Color
-            $change = str_replace("rgb(255,255,0)", MAIN_COLOR_BACK['main'], $change);
-            $change = str_replace("rgb(255,229,153)", MAIN_COLOR_BACK['minor'], $change);
-            $change = str_replace("rgb(43,120,228)", MAIN_COLOR_BACK['road'], $change);
-            $change = str_replace('font-family: balsamic'
-                , 'font-family: lalezar', $change);
-            file_put_contents("$check", "$change", LOCK_EX);
-        }
+        #SWAP COLOR
+        [
+            &$MAIN_COLOR_CHECK['Recommend'][0] , &$MAIN_COLOR_CHECK['Recommend'][1],
+            $MAIN_COLOR_CHECK['Alternative'][0], $MAIN_COLOR_CHECK['Alternative'][1] ,
+            $MAIN_COLOR_CHECK['No'][0],$MAIN_COLOR_CHECK['No'][1],
+            $MAIN_COLOR_BACK['Main'][0],$MAIN_COLOR_BACK['Main'][1] ,
+            $MAIN_COLOR_BACK['Minor'][0], $MAIN_COLOR_BACK['Minor'][1] ,
+            $MAIN_COLOR_BACK['Road'][0] , $MAIN_COLOR_BACK['Road'][1]
+        ] = [
+            &$MAIN_COLOR_CHECK['Recommend'][1] , &$MAIN_COLOR_CHECK['Recommend'][0],
+            $MAIN_COLOR_CHECK['Alternative'][1], $MAIN_COLOR_CHECK['Alternative'][0] ,
+            $MAIN_COLOR_CHECK['No'][1],$MAIN_COLOR_CHECK['No'][0],
+            $MAIN_COLOR_BACK['Main'][1],$MAIN_COLOR_BACK['Main'][0] ,
+            $MAIN_COLOR_BACK['Minor'][1], $MAIN_COLOR_BACK['Minor'][0] ,
+            $MAIN_COLOR_BACK['Road'][1] , $MAIN_COLOR_BACK['Road'][0]
+        ];
+
+        var_dump($MAIN_COLOR_CHECK);
+
+//        file_put_contents("$check", "$change", LOCK_EX);
     }
     readfile($check);
 }
